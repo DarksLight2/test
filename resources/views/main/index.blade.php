24fdf8f2d29705ba9628ddaaa4729cb728d80ea5
Maksym Kovalets <darkslight222@gmail.com>
2022-08-13 23:18:43 +0200

    a

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,23 @@
+<x-default-layout :title="$title">
+    <div class="container">
+        <section class="wellcome">
+            <div class="wellcome__left">
+                <h2>
+                    HELLO
+                </h2>
+                <h1>
+                    I’m Gleb Kostrubov
+                </h1>
+                <p>
+                    I've been doing web design, front-end and back-end development for a year now. Do you need a website design, site layout, or maybe a turnkey website? Then contact me
+                </p>
+                <button>
+                    Contact Me
+                </button>
+            </div>
+            <div class="wellcome__right">
+
+            </div>
+        </section>
+    </div>
+</x-default-layout>
